498b6400bd77eb06940fa67949e75ecf4891109b
Nanda Sankaran <nanda@mobme.in>
2012-02-27 20:53:54 +0530

    Converting to an Phonegap application

---
+++ src/in/mobme/tvtickerlite/TvTickerLiteActivity.java
@@ -0,0 +1,16 @@
+package in.mobme.tvtickerlite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class TvTickerLiteActivity extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+	super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
